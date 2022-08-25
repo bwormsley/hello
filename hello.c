dd12055b4cb4c205e050901ee7ffe46059b1c722
bwormsley <bwormsley2@huskers.unl.edu>
2022-08-25 18:15:20 +0000

    added major description, showed fellow students git commands, fixed a syntax error, unloaded a unused library

--- hello.c
+++ hello.c
@@ -1,13 +1,14 @@
-// Benjamin Wormsley
-// 08/24/2022
- // Write information about myself
+/** Benjamin Wormsley
+ *  08/24/2022
+ *  Write information about myself
+ */
 
 
 #include <stdio.h>
 
 int main(int argc, char **argv) {
 
-    printf("My name is Benjamin Wormsley.\nI am ");
+    printf("My name is Benjamin Wormsley.\nI am majoring in Computer Engineering.");
 
     return 0;
 }
