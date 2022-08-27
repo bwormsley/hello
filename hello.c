f073fb186ea6bf68763ddedd9b8cd8961d115cd8
bwormsley <bowrmsley2@huskers.unl.edu>
2022-08-27 19:24:22 +0000

    fixed some formatting issues

--- hello.c
+++ hello.c
@@ -8,7 +8,7 @@
 
 int main(int argc, char **argv) {
 
-    printf("My name is Benjamin Wormsley.\nI am majoring in Computer Engineering.");
+    printf("My name is Benjamin Wormsley.\nI am majoring in Computer Engineering.\n");
 
     return 0;
 }
